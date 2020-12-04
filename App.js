79cc29411c56259d4ce3f682f60bfa3fdb621aac
Richard See <richard.see@argos.co.uk>
2020-12-04 18:36:24 +0000

    Styling

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
         <Text>Hello world!</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+// Can do inline styles, but StyleSheet is better as it caches the styles
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
 
 export default App;
